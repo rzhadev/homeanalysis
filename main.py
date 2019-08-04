38f9d45c801b9e6a4592d084c78460726d13f1c4
rzha1 <44590108+rzha1@users.noreply.github.com>
2019-08-04 10:59:16 -0400

    update

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import seaborn as sns 
 import matplotlib.pyplot as plt
-df = pd.read_csv('USA_Housing.csv')
-plot = sns.scatterplot(x='Avg. Area Income',y='Price',data=df)
-plt.show(sns)
+import neuralnetwork as nn
+import math
+
+brain = nn.NeuralNetwork(2,2,1)
+input = [1,0]
+output = brain.feedforward(input,lambda x: 1 / (1 + math.exp(-x)))
+print(output)
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,6 +1,23 @@
+import numpy as np
+import math
 class NeuralNetwork:
-    def __init__(self,input,hidden,output):
+    def __init__(self, input, hidden, output):
         self.inputNodes = input
         self.hiddenNodes = hidden
         self.outputNodes = output
-    
+        self.ih_weights = np.random.random(size=(self.hiddenNodes, self.inputNodes))*2 - 1
+        self.oh_weights = np.random.random(size=(self.outputNodes, self.hiddenNodes))*2 - 1
+        self.h_bias = np.random.random()*2-1
+        self.o_bias = np.random.random()*2-1
+    def feedforward(self, input, activation):
+        inputs = np.array(input)
+        hidden = self.ih_weights @ inputs
+        hidden += self.h_bias
+        # activation function
+        hidden = np.array(list(map(activation, hidden)))
+        output = self.oh_weights @ hidden
+        output += self.o_bias
+        output = np.array(list(map(activation, output)))
+        return output.tolist()
+    def train(self):
+        pass
